f8006c31ef6a6792d8360002de60576e2874f80a
Ahilya Pawar <ahilya.pawar09@gmail.com>
2024-02-05 17:42:49 +0530

    React Element and jsx

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-// const heading = React.createElement("h1",{
-//     id:"heading",
-//     xyz:"abc"
-// },"Hello World Form React !");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading)
 
+ // This is old way to create Dom elements
+
+/*const heading = React.createElement("h1",{ id:"heading",xyz:"abc"},"Hello World Form React !");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)*/
+
+
+//using jsx
+
+/*const jsxheading = (<h1 id="heading">
+                       Namaste React Using jsx!.
+                    </h1>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading)*/
+
+
+//React Element
+const heading = <h1>This is React Element</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
+
+// React Component
+//React functional component
+
+const HeadingComponent = ()=>{
+    return(
+        <div>
+            <h1 className='heading'>This is functional component</h1>
+        </div>
+    )
+}
+
+const HeadingComponent2 =()=>(
+    <div>
+        <h1>This is heading component2</h1>
+    </div>
+)
+//both above are same code
 
 
 // we built this nested div
@@ -22,23 +56,23 @@
     </div>
 */
 
-const parent = React.createElement("div",{id:"parent"},
-            [
-               React.createElement("div",{id:"child"},
-               [
-               React.createElement("h1",{},"I am in h1 tag"),
-               React.createElement("h2",{},"I am in h2 tag"),
-               ]
-               ),
-               React.createElement("div",{id:"child2"},
-               [
-               React.createElement("h1",{},"I am in h1 tag"),
-               React.createElement("h2",{},"I am in h2 tag"),
-               ]
-               )
-            ]
-               );
+// const parent = React.createElement("div",{id:"parent"},
+//             [
+//                React.createElement("div",{id:"child"},
+//                [
+//                React.createElement("h1",{},"This is Namaste React "),
+//                React.createElement("h2",{},"I am in h2 tag"),
+//                ]
+//                ),
+//                React.createElement("div",{id:"child2"},
+//                [
+//                React.createElement("h1",{},"kadkfskdsjfdkfjkldfsds tag"),
+//                React.createElement("h2",{},"I am in h2 tag"),
+//                ]
+//                )
+//             ]
+//                );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent)
+// root.render(parent)
